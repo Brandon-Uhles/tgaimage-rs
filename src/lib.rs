7e573f964323dc1f2bfa821ebb089da7d6103389
Brandon-Uhles <brandon.uhles@gmail.com>
2024-05-13 15:29:40 -0400

    feat(constructor): add constructors for various desirable configs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct TGAImage {
+    width: i32,
+    height: i32, 
+    bytespp: i32,
+    data: Vec<u8>,
+}
+
+impl TGAImage {
+
+    // Constructor with default values
+    pub fn new() -> Self {
+        TGAImage {
+            width: 0, 
+            height: 0, 
+            bytespp: 0, 
+            data: vec![],
+        }
+    }
+
+    //Constructor with provided dimensions, no data
+    pub fn with_dimensions(width: i32, height: i32, bytespp: i32) -> Self {
+        let data_size = (width * height * bytespp) as usize;
+
+        TGAImage {
+            width, 
+            height, 
+            bytespp, 
+            data: vec![0; data_size],
+        }
+    }
+
+    pub fn with_data(width: i32, height: i32, bytespp: i32, data: Vec<u8>) -> Self {
+        TGAImage {
+            width,
+            height,
+            bytespp,
+            data,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +45,5 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
